727599ff6f35f8639386620d81fe3d8647b69d25
‘terupro’ <terupro911@gmail.com>
2022-08-10 15:59:26 +0900

    ⚡️ 検索機能の実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { MicroCMSListResponse } from "microcms-js-sdk";
 import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import Link from "next/link";
+import { ComponentProps, FormEventHandler, useState } from "react";
 import { client } from "src/libs/client";
 
 // propsの型を定義
@@ -21,14 +22,48 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
 };
 
 const Home: NextPage<Props> = (props) => {
+  const [search, setSearch] = useState<MicroCMSListResponse<Blog>>();
+  const handleSubmit: ComponentProps<"form">["onSubmit"] = async (e) => {
+    e.preventDefault();
+    const q = e.currentTarget.query.value;
+    // ここの処理はブラウザ側でも起こってしまうため、サーバー側で処理を行う
+    // API Routes(簡易的にサーバー側の処理をかけるもの)に入力した値を渡す
+    const data = await fetch("/api/search", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ q }),
+    });
+    const json: MicroCMSListResponse<Blog> = await data.json();
+    setSearch(json);
+  };
+
+  const handleClick: ComponentProps<"button">["onClick"] = () => {
+    setSearch(undefined);
+  };
+
+  const contents = search ? search.contents : props.contents;
+  const totalCount = search ? search.totalCount : props.totalCount;
   return (
     <div>
-      <p className="text-gray-500">{`記事の総数: ${props.totalCount}`}</p>
+      <form className="flex gap-x-2" onSubmit={handleSubmit}>
+        <input type="text" name="query" className="border border-black px-2" />
+        <button className="border border-black px-2">検索</button>
+        <button
+          onClick={handleClick}
+          type="reset"
+          className="border border-black px-2"
+        >
+          リセット
+        </button>
+      </form>
+      <p className="mt-4 text-gray-500">{`${
+        search ? "検索結果" : "記事の総数"
+      }: ${totalCount}`}</p>
       <ul className="mt-4 space-y-4">
-        {props.contents.map((content) => {
+        {contents.map((content) => {
           return (
             <li key={content.id}>
-              <Link href={`blog/${content.id}`}>
+              <Link href={`/blog/${content.id}`}>
                 <a className="text-xl underline hover:opacity-50">
                   {content.title}
                 </a>
