4737f5cad253a4fc601c075c6ef7cdecfd14da0f
‘terupro’ <terupro911@gmail.com>
2022-08-11 10:51:01 +0900

    ✨ headの追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import "src/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="mx-auto my-8 max-w-prose">
+      <Head>
+        <title>TERUBLOG</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <div className="mx-4">
         <header className="border-b border-green-300 pb-8">
           <h1 className="text-center">
